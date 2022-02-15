f7de71175869e12a9b95a53b08b2849e4806be8a
wlm <599290100@qq.com>
2022-02-15 18:07:38 +0900

    spring security
    login

--- src/main/java/book/manager/controller/pageController.java
+++ src/main/java/book/manager/controller/pageController.java
@@ -3,9 +3,7 @@ package book.manager.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-/**
- * 专用于处理页面响应的控制器
- */
+
 @Controller
 public class pageController {
     @RequestMapping("/index")
